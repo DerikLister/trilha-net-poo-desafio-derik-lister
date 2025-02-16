36099e06495bf126ea70c82ae0bfe06cfef08400
Derik Lister <derik.1997@outlook.com.br>
2025-02-16 20:35:05 -0300

    Desfio Concluído
    Objetivos do desafio concluídas.
    - Classes Nokia e Iphone implementadas .
    - Novo método criado para exibir informações das classes filhas criadas.
    - Testes Realizados.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("1188888880", "Nokia tijolao", "1234576", 12);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("ZipZop");
+        nokia.Informações();
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("11958858366", "iPhone11", "987654321", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Apple Store");
+        iphone.Informações();
+
+
+        Console.ReadLine();
+    }
+}
